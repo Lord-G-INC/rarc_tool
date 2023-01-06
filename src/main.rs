950471bd76c0992965154fc6ff75cfc21ddcb3b7
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-01-05 18:09:21 -0600

    finish stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 mod rarc;
 mod seektask;
 
+use std::path::*;
+use std::env;
+
 
 fn main() {
-    let mut data = std::fs::read("AbekobeGalaxyMap.arc").unwrap_or_default();
-    data = yaz0rust::decompress(&data);
-    let rarc = rarc::RARC::read(data);
-    rarc.extract();
+    let args = env::args().collect::<Vec<String>>();
+    let files = args.iter().map(|x| Path::new(x))
+    .filter(|x| x.exists() && x.is_file()).collect::<Vec<_>>();
+    for file in files {
+        let mut data = std::fs::read(file).unwrap_or_default();
+        data = yaz0rust::decompress(&data);
+        let rarc = rarc::RARC::read(data);
+        std::env::set_current_dir(file.parent().unwrap()).unwrap();
+        rarc.extract();
+    }
 }
