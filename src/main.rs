42ec8bfd51fa3e57f517905265ee4104b3bf2654
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-01-08 16:15:39 -0600

    a cpp for packing will be needed

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
     let files = args.iter().skip(1).map(|x| Path::new(x))
     .filter(|x| x.exists() && x.is_file()).collect::<Vec<_>>();
-    let dirs = args.iter().skip(1).map(|x| Path::new(x))
-    .filter(|x| x.exists() && x.is_dir()).collect::<Vec<_>>();
     for file in files {
         let mut data = std::fs::read(file).unwrap_or_default();
         data = yaz0rust::decompress(&data);
@@ -23,18 +21,5 @@ fn main() {
             }
         }
         rarc.extract();
-    }
-    for dir in dirs {
-        let mut r = rarc::RARC::default();
-        let attr = rarc::FileAttr::FILE | rarc::FileAttr::LOADTOMRAM;
-        r.importfromfolder(dir.to_string_lossy().as_ref(), attr);
-        let mut c = vec![];
-        for item in r.dirs {
-            if item.name == "." || item.name == ".." {
-                continue;
-            }
-            c.push(item);
-        }
-        println!("{}", c.len());
     }
 }
